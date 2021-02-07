e9c17acd3ebb5f7acd5249283ab8e8dae5072fd7
himanshunete <himanshuneteh@gmail.com>
2021-02-07 10:48:21 +0530

    [himanshunete]Add FirstCommit

---
+++ TicTacToeApplication/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+
+namespace TicTacToeApplication
+{
+    class TicTactoe
+    {
+        public static string PLAYER_1;
+        public static char[] board = { ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ' };
+        public static void createBoard()
+        {
+            for (int i = 1; i < 10; i++)
+            {
+                Console.WriteLine(" Empty board:" + i + ": "+ board[i] );
+            }
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" Welcome to Tic Tac Toe Game");
+            TicTactoe.createBoard();
+        }
+    }
+
+}
